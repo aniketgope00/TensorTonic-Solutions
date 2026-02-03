0a068992fb5eb0c5cd7f3101bbcf5d44ca762ebc
Aniket Gope <87859558+aniketgope00@users.noreply.github.com>
2026-02-03 16:43:02 +0530

    Add solution for simple-moving-average

---
+++ simple-moving-average/simple-moving-average.py
@@ -0,0 +1,11 @@
+def simple_moving_average(values, window_size):
+    """
+    Compute the simple moving average of the given values.
+    """
+    # Write code here
+    n           = len(values)
+    result      = []
+    for i in range(n - window_size + 1):
+        val = sum(values[i:i + window_size])/window_size
+        result.append(val)
+    return result
