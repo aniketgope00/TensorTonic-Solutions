faf087cfbc66416d88d3005fcd845dab9bd6c0eb
Aniket Gope <87859558+aniketgope00@users.noreply.github.com>
2026-03-12 02:27:01 +0530

    Update solution for leaky-relu

--- leaky-relu/leaky-relu.py
+++ leaky-relu/leaky-relu.py
@@ -5,5 +5,6 @@ def leaky_relu(x, alpha=0.01):
     Vectorized Leaky ReLU implementation.
     """
     # Write code here
-    x = np.asarray(x)
-    return np.where(x >= 0, x, alpha * x)
+    x               = np.asarray(x)
+    result = np.where(x >=0, x, alpha * x)
+    return result
